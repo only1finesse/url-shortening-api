874a06db41f222539198a9fff9cbfa9d84cd52ea
only1finesse <72461240+only1finesse@users.noreply.github.com>
2023-03-20 20:57:15 -0700

    fixes hero section

--- src/components/hero/RightHero.js
+++ src/components/hero/RightHero.js
@@ -7,9 +7,7 @@ const RightHero = () => {
     return (
         <div className='heroSection'>
             <div className='flex'>
-                <Image className='object-none scale-75 md:ml-14 ml-24 overflow-visible md:object-left md:scale-105' src={working} />
-                {/* <Image className='ml-24 object-none md:object-left overflow-visible scale:75 md:scale-100' src={working} /> */}
-
+                <Image className='object-none scale-75 md:ml-17 ml-24 overflow-visible md:object-left md:scale-105' src={working} />
             </div>
         </div>
     )
